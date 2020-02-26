aae1967a992cdefa3e240494cab1f277faafc3ad
Pekka Nykänen <pekka.nykanen@outlook.com>
2020-02-26 22:29:33 +0200

    Viikko 4, tehtavat3-4

---
+++ viikko4/tehtava4/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.verkkokauppa;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class KauppaTest {
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    
+    @Before
+    public void setUp(){
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+        varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.saldo(3)).thenReturn(0);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 15));
+        when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "yksisarvinen", 1500));
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());
+    }
+    
+    @Test
+    public void ostosVeloitetaanOikealtaAsiakkaalta(){
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"),eq(5));
+    }
+    
+    @Test
+    public void ostetaanKaksiEriTuotettaVarastostaJaVeloitetaanOikein(){
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"),eq(20));
+    }
+    
+    @Test
+    public void ostetaanKaksiSamaaTuotettaVarastostaJaVeloitetaanOikein(){
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"),eq(10));
+    }
+    
+    @Test
+    public void ostetaanKaksiEriTuotettaToistaEiOleJaVeloitetaanOikein(){
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(3);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"),eq(5));
+    }
+    
+    @Test
+    public void alustusNollaaOstoskorin(){
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(3);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"),eq(5));
+    }
+    
+    @Test
+    public void viiteKysytaanMaksutapahtumanYhteydessa(){
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+        k.lisaaKoriin(3);
+        k.tilimaksu("pekka", "12345");
+        verify(viite, times(3)).uusi();
+    }
+    
+    @Test
+    public void koristaPoistoOnnistuu(){
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),eq(15));
+    }
+}
