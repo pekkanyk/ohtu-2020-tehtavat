aed8c6027be9b341937bbfe9aa722f32782aef67
Pekka Nykänen <pekka.nykanen@outlook.com>
2020-03-03 06:11:15 +0200

    Viikko 5, tehtävä 5

---
+++ viikko5/tehtava5/src/main/java/laskin/Summa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author qru19
+ */
+public class Summa extends Komento{
+    public int valueForUndo;
+
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+        this.valueForUndo = sovellus.tulos();
+    }
+    
+
+    @Override
+    public void suorita(){
+        try {
+            int arvo = Integer.parseInt(syotekentta.getText());
+            valueForUndo = sovellus.tulos();
+            sovellus.plus(arvo);
+            syotekentta.setText("");
+            tuloskentta.setText("" + sovellus.tulos());
+            nollaa.disableProperty().set(false);
+            undo.disableProperty().set(false);
+        } catch (Exception e) {
+        }
+        
+    }
+
+    @Override
+    public void peru() {
+        syotekentta.setText("");
+        tuloskentta.setText(""+valueForUndo);
+        sovellus.setTulos(valueForUndo);
+        nollaa.disableProperty().set(false);
+        undo.disableProperty().set(true);
+    }
+    
+}
