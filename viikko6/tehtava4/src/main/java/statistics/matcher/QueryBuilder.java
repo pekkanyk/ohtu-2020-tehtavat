e59d5aa2f3414186008734d3922c8a54d280a491
Pekka Nykänen <pekka.nykanen@outlook.com>
2020-03-05 19:33:20 +0200

    Viikko 6, Tehtävä 4

---
+++ viikko6/tehtava4/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+/**
+ *
+ * @author qru19
+ */
+public class QueryBuilder {
+    Matcher matcher;
+    
+    public QueryBuilder(){
+        matcher = new All();
+    }
+    
+    public Matcher build(){
+        return matcher;
+    }
+    
+    public QueryBuilder hasAtLeast(int value, String category){
+        this.matcher = new And(this.matcher,new HasAtLeast(value,category));
+        //this.matcher = new HasAtLeast(value, category);
+        return this;
+    }
+    
+    public QueryBuilder playsIn(String team){
+        this.matcher = new And(this.matcher,new PlaysIn(team));
+        return this;
+    }
+    
+    public QueryBuilder hasFewerThan(int value, String category){
+        this.matcher = new And(this.matcher,new HasFewerThan(value, category));
+        return this;
+    }
+}
